93508761c82dc3a4e0acfdba7876f3fc9b45ef1b
samersamyr44-web <samersamyr44@gmail.com>
2026-08-09 22:56:32 +0300

    Update backend for deployment

--- server/index.js
+++ server/index.js
@@ -4,30 +4,14 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
-
 const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
-
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection failed:", error.message);
-  });
 
 // Test route
 app.get("/", (req, res) => {
@@ -35,3 +19,25 @@ app.get("/", (req, res) => {
     message: "Cartify API is running",
   });
 });
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error.message);
+  });
+
+// Export app for Vercel
+module.exports = app;
+
+// Run server locally
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
